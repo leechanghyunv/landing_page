2116e58b04753c6b98a198ef955598091758372f
leechanghyunv <qqqqq7ppppp@gmail.com>
2025-02-17 17:32:50 +0900

    first project

---
+++ src/app/header.js
@@ -0,0 +1,10 @@
+
+<div className="my-4">
+  {/* 헤더 */}
+  <div className="flex justify-between sm:justify-center space-x-4">
+    <div className="text-lg font-bold">홈</div>
+    <div className="text-lg font-bold">기능</div>
+    <div className="text-lg font-bold">가격</div>
+    <div className="text-lg font-bold">문의</div>
+  </div>
+</div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,126 @@
-import Image from "next/image";
+'use client'
+
+import Image from 'next/image';
+import { texts,appStore } from './text'; 
+import React, { useState, useEffect } from "react";
+
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const handleAndroidClick = () => {
+    alert("준비중입니다!");
+  };
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+  return (
+    <div>
+
+
+<header className="w-full text-white py-4" style={{ backgroundColor: '#EAEAEA' }}>
+      <div className="flex justify-end items-center mx-4">
+        <nav>
+          <ul className="flex space-x-4 text-black font-bold ">
+            <li><a href={appStore} 
+            onClick={(e) => {
+
+            }} className="text-base lg:text-xl">IOS</a></li>
+            <li>
+              <a 
+                href="#reviews" 
+                onClick={(e) => { 
+                  e.preventDefault();  // 기본 링크 동작을 막고
+                  handleAndroidClick(); // 준비중 팝업 띄우기
+                }}
+                className="text-base lg:text-xl"
+              >
+                Android
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+
+
+
+
+
+
+  <div className='flex justify-center'>
+       <img
+    src="/assets/공수계산기_워크캘린더.png"
+    alt="워크캘린더 공수계산기"
+    className="w-auto h-auto my-4 md:hidden"
+  />
+<img
+  src="/assets_big/공수계산기_소개.png" // 모바일 이상에서 보일 이미지
+  alt="워크캘린더 공수계산기"
+  className="hidden md:block w-full h-auto my-4" // 모바일 이상에서 보이도록 설정
+/>
+  </div>
+      
+
+  <div className='flex justify-center my-4'>
+       <img
+    src="/assets/소비자_리뷰.png"
+    alt="워크캘린더 공수계산기"
+    className="w-auto h-auto my-4 md:hidden"
+  />
+<img
+  src="/assets_big/공수달력_특징.png" // 모바일 이상에서 보일 이미지
+  alt="워크캘린더 공수계산기"
+  className="hidden md:block w-full h-auto my-4" // 모바일 이상에서 보이도록 설정
+/>
+  </div>
+      
+ <div className='flex justify-center my-4'>
+       <img
+    src="/assets/편리한_공수등록.png"
+    alt="워크캘린더 공수계산기"
+    className="w-auto h-auto my-8 md:hidden"
+  />
+
+  </div>
+
+
+<div className="my-4">
+  {/* 스와이프 가능한 이미지 목록 */}
+     <img
+    src="/assets/공수기록_백업.png"
+    alt="워크캘린더 공수계산기"
+    className="w-auto h-auto my-4 md:hidden"
+  />
+
+  <img
+  src="/assets_big/공수_특징.png" // 모바일 이상에서 보일 이미지
+  alt="워크캘린더 공수계산기"
+  className="hidden md:block w-full h-auto my-4" // 모바일 이상에서 보이도록 설정
+/>
+
+</div>
+
+
+ <div className='flex justify-center my-2'>
+       <img
+    src="/assets/Q&A.png"
+    alt="워크캘린더 공수계산기"
+    className="w-auto h-auto my-4 md:hidden"    
+  />
+  <img
+  src="/assets_big/공수어플_평가.png" // 모바일 이상에서 보일 이미지
+  alt="워크캘린더 공수계산기"
+  className="hidden md:block w-full h-auto my-4" // 모바일 이상에서 보이도록 설정
+/>
+
+  </div>
+
+
+
     </div>
   );
 }
+
+
+
+
+
