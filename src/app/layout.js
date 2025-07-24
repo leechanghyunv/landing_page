aaad9abfcdcbcd0cb38309347c677c166d96ce67
leechanghyunv <qqqqq7ppppp@gmail.com>
2025-07-25 01:53:03 +0900

    250725

--- src/app/layout.js
+++ src/app/layout.js
@@ -40,6 +40,9 @@ export default function RootLayout({ children }) {
     <head>
     <meta charSet="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+    
+    <meta name="naver-site-verification" content="2072f8e87baa3a250aebcafae511f324e6047c49" />
+
     <title>App Landing Page</title>      
     <style> 
     </style>
