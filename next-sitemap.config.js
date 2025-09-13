e7018c47915e8b914617afc2217f91fd42a02ef1
leechanghyunv <qqqqq7ppppp@gmail.com>
2025-09-13 22:33:17 +0900

    250913-4

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,6 @@
 // next-sitemap.config.js
 /** @type {import('next-sitemap').IConfig} */
+
 module.exports = {
   siteUrl: 'https://landing-page-vert-sigma.vercel.app', // ← 실제 배포 주소
   generateRobotsTxt: true, // robots.txt도 생성
